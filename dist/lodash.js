8f51195a983c52e449817961ed2e00d6b64537fa
Hugo ALLIAUME <kocal@live.fr>
2016-05-17 10:52:21 +0200

    Doc: add doc for WebSocket.constructor()

--- dist/lodash.js
+++ dist/lodash.js
@@ -16241,27 +16241,3 @@
     root._ = _;
   }
 }.call(this));
-
-var WebSocket;
-
-WebSocket = (function() {
-
-  /**
-   * Initialize a new WebSocket object with given options.
-   * @param {object}   options         Object options
-   * @param {string}   options.host    Host used for connection
-   * @param {integer}  options.port    Port user for connection
-   * @param {bool}     options.secure  Using ws or wss protocol
-   */
-  function WebSocket(options) {
-    this.options = _.merge({
-      host: 'localhost',
-      port: 8000,
-      secure: false
-    }, options);
-    return;
-  }
-
-  return WebSocket;
-
-})();
--- src/websocket.coffee
+++ src/websocket.coffee
@@ -1,4 +1,11 @@
 class WebSocket
+    ###*
+    # Initialize a new WebSocket object with given options.
+    # @param {object}   options         Object options
+    # @param {string}   options.host    Host used for connection
+    # @param {integer}  options.port    Port user for connection
+    # @param {bool}     options.secure  Using ws or wss protocol
+    ###
     constructor: (options) ->
         @options = _.merge {
             host: 'localhost',
