557f5b7b46c627fc3f6af640e87f82be3374b2a1
Hugo ALLIAUME <kocal@live.fr>
2016-05-19 11:59:45 +0200

    Test: add tests for server connection, gonna work on it again

--- tests/tornado_websocket.js
+++ tests/tornado_websocket.js
@@ -208,24 +208,101 @@ describe('`TornadoWebSocket::connect()`', function () {
     });
 
     it('should connect to server and got user defined error message', function (done) {
-        var ws = new TornadoWebSocket('/my_chat', {
+        var ws = new TornadoWebSocket('/my_chat_without_history', {
             host: 'kocal.fr'
         });
 
         ws.on('open', function (socket, event) {
+            // 'error' is here an user-made event and not a reserved one, see ws.on('error') right under
             socket.on('error', function (data) {
-                console.log('DATA=======', data);
-                expect(data).toEqual(jasmine.any(Object))
+                expect(data).toEqual(jasmine.any(Object));
+                expect(data.message).toEqual(jasmine.any(String));
+
+                // Maybe I should rework django-tornado-server events, to make a distinction between
+                // reserved events (open, error and close) and user-made event, because it can be confusing...
+                //
+                // In a next version, we should expect a message like this "The user-defined event "open" does
+                // not exist for websocket "<tornado_websockets_....>".
                 expect(data.message).toContain('The event "open" does not exist for websocket');
                 done();
             });
         });
 
         ws.on('error', function (error) {
-            throw new Error('Should not be executed')
+            // Since 'error' event is sent by tornado_websockets.websockethandler.WebSocketHandler.emit('error'),
+            // it's not the real 'onerror' WebSocket event but an user-made one.
+            // So the following code will not be executed.tt
+            throw new Error('Should not be executed');
         });
 
         ws.connect();
+    });
+
+    it('should connect to server and test `emit("message")` and `on("new_message")`', function (done) {
+        // See https://github.com/Kocal/django-tornado-websockets/blob/master/testapp/views.py
+        var ws = new TornadoWebSocket('/my_chat_without_history', { host: 'kocal.fr' });
+
+        ws.on('open', function (socket) {
+            socket.emit('connection', { username: 'User' });
+
+            socket.on('new_connection', function (data) {
+                expect(data.message).toBe('User just joined the webchat.');
+
+                socket.emit('message', {
+                    username: 'User',
+                    message: 'My message'
+                });
+            });
+
+            socket.on('new_message', function (data) {
+                expect(data.username).toBe('User');
+                expect(data.message).toBe('My message');
+                // Force close
+                ws.websocket.close();
+                done();
+            });
+        });
+
+        ws.connect();
+    });
+
+    it('should connect to server and start a conversation between two users in a broadcast room', function (done) {
+
+        // Hollywood-quality scenario:
+        // - User #1: Hi
+        // - User #2: Hi User #1
+        // - User #1: Bye User #1
+        // - User #2: kthxbye
+
+        var client1 = new TornadoWebSocket('/my_chat_without_history', { host: 'kocal.fr' });
+        var client2 = new TornadoWebSocket('/my_chat_without_history', { host: 'kocal.fr' });
+
+        client1.on('open', function (socket) {
+            socket.emit('connection', { username: 'User #1' });
+
+            socket.on('new_connection', function (data) {
+                expect(data.message).toBe('User #1 just joined the webchat.');
+
+                // Override new_connection event
+                socket.on('new_connection', function(data) {
+                    expect(data.message).toBe('User #2 just joined the webchat.');
+                });
+
+                // Connect client 2
+                client2.connect();
+            });
+        });
+
+        client2.on('open', function (socket) {
+            socket.emit('connection', { username: 'User #2' });
+
+            socket.on('new_connection', function (data) {
+                expect(data.message).toBe('User #2 just joined the webchat.');
+                done();
+            });
+        });
+
+        client1.connect();
     });
 
 });
